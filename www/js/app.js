a5494450479080608a24477690bde76d3d9a0afb
mens-rea <juan.miguel.g.alvarez@gmail.com>
2016-10-06 20:43:18 -0700

    trying sqlite still

--- www/js/app.js
+++ www/js/app.js
@@ -33,17 +33,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         /*db = window.openDB({ name: 'populated.db',location: 'default' });*/
         db = window.openDatabase("populated.db", 0, 'populated', 1024 * 1024 * 100);
 
-        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY, docname TEXT, prog INTEGER, proc INTEGER)").then(function(res) {
+        /*$cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY, docname TEXT, prog INTEGER, proc INTEGER)").then(function(res) {
           alert("inserted!"+db);  
         }, function (err) {
           alert("error1:"+err.message);
-        });
-        var query = "INSERT INTO documents (docname, prog, proc) VALUES ('death',0,3)";
+        });*/
+        /*var query = "INSERT INTO documents (docname, prog, proc) VALUES ('death',0,3)";
         $cordovaSQLite.execute(db, query).then(function(res) {
           alert("inserted!"+docname + " " + prog);  
         }, function (err) {
           alert("error1:"+err.message);
-        });
+        });*/
         alert("success");
       } catch (error) {
         alert(error);
