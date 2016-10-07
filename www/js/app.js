e2b8bc636b91e9411c053bb52745ebcb16826ec1
mens-rea <juan.miguel.g.alvarez@gmail.com>
2016-10-07 01:48:22 -0700

    sqlite trials

--- www/js/app.js
+++ www/js/app.js
@@ -32,12 +32,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         /*db = window.sqlitePlugin.openDB({name:"populated.db",location:'default'});*/
         /*db = window.openDB({ name: 'populated.db',location: 'default' });*/
 
-          window.plugins.sqlDB.copy("populated.db", 0, function() {
+          /*window.plugins.sqlDB.copy("populated.db", 0, function() {
               db = window.openDatabase("populated.db", 0, 'populated', 1024 * 1024 * 100);
           }, function(error) {
               console.error("There was an error copying the database: " + error);
               db = window.openDatabase("populated.db", 0, 'populated', 1024 * 1024 * 100);
-          });
+          });*/
+
+          db = window.openDatabase("populated.db", 0, 'populated', 1024 * 1024 * 100);
 
           /*var s_query = "SELECT * FROM documents";
           $cordovaSQLite.execute(db, s_query).then(function(res) {
@@ -54,17 +56,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
             });*/
 
             $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY, docname TEXT, prog INTEGER, proc INTEGER)").then(function(res) {
-            var db_path = context.getDatabasePath("populated.dbg").getPath();
+            var db_path = context.getDatabasePath("populated.db").getPath();
             alert("inserted!"+db + " " + db_path);  
         }, function (err) {
             alert("error1:"+err.message);
         });
-        var query = "INSERT INTO documents (docname, prog, proc) VALUES ('death',0,3)";
+        /*var query = "INSERT INTO documents (docname, prog, proc) VALUES ('death',0,3)";
         $cordovaSQLite.execute(db, query).then(function(res) {
           alert("inserted!"+docname + " " + prog);  
         }, function (err) {
           alert("error1:"+err.message);
-        });
+        });*/
       } catch (error) {
         alert(error);
       }
